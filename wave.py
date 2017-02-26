bf672f1764b712b1dc517797de461dc5b9bc5c2d
Alexey Roslyakov <alexey.roslyakov@newsycat.com>
2017-02-26 20:31:52 +0700

    Polish wave.py using PEP8 wax

--- wave.py
+++ wave.py
@@ -1,42 +1,35 @@
 #!/usr/bin/python
-import spidev
-import ws2812
+"""
+'Wave' effect for WS2812B led strip
+"""
+
 import time
-import getopt
 import numpy
 from numpy import sin, pi
+import spidev
+import ws2812
 
-def test_pattern_sin(spi, nLED=8, intensity=20):
-    tStart=time.time()
-    indices=4*numpy.array(range(nLED), dtype=numpy.uint32)*numpy.pi/nLED
-    period0=2
-    period1=2.1
-    period2=2.2
+def _test_pattern_sin(_spi, num_leds=8, intensity=20):
+    start_time = time.time()
+    indices = 4 * numpy.array(range(num_leds), dtype=numpy.uint32) * numpy.pi / num_leds
+    period0 = 2
+    period1 = 2.1
+    period2 = 2.2
     try:
         while True:
-            t=tStart-time.time()
-            #t=1.1
-            f=numpy.zeros((nLED,3))
-            f[:,0]=sin(2*pi*t/period0+indices)
-            f[:,1]=sin(2*pi*t/period1+indices)
-            f[:,2]=sin(2*pi*t/period2+indices)
-            f=(intensity)*((f+1.0)/2.0)
-            fi=numpy.array(f, dtype=numpy.uint8)
-            #print fi[0]
-            #time_write2812(spi, fi)
-            ws2812.write2812(spi, fi)
+            _time = start_time - time.time()
+            f = numpy.zeros((num_leds, 3))
+            f[:, 0] = sin(2 * pi * _time / period0 + indices)
+            f[:, 1] = sin(2 * pi * _time / period1 + indices)
+            f[:, 2] = sin(2 *pi * _time / period2 + indices)
+            f = intensity * ((f + 1.0) / 2.0)
+            fi = numpy.array(f, dtype=numpy.uint8)
+            ws2812.write2812(_spi, fi)
             time.sleep(0.01)
     except KeyboardInterrupt:
-        test_off(spi, nLED)
+        ws2812.off_leds(_spi, num_leds)
 
-def test_off(spi, nLED):
-    ws2812.write2812(spi, [[0, 0, 0]] * nLED)
-
-if __name__=="__main__":
+if __name__ == "__main__":
     spi = spidev.SpiDev()
-    spi.open(0,0)
-    
-    test_pattern_sin(spi, nLED=8, intensity=255)
-    #test_fixed(spi)
-
-
+    spi.open(0, 0)
+    _test_pattern_sin(spi, num_leds=8, intensity=255)
